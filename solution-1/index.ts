81aa0cf29be29ad927c32b3ec6e71bbecf17b3d4
Thiago <ti.thiago.rodrigues@gmail.com>
2026-01-30 22:12:01 -0300

    finish solution one

--- solution-1/index.ts
+++ solution-1/index.ts
@@ -2,15 +2,34 @@
 
 export function twoSum(nums: number[], target: number): number[] {
   let result: number[] = [];
-  nums.forEach((num, indexListOne) => {
-    nums.forEach((numSecond, indexListTwo) => {
-      const sum = num + numSecond;
+
+  for (const [firstIndex, firstItem] of nums.entries()) {
+    for (const [secondIndex, secondItem] of nums.entries()) {
+      const sum = firstItem + secondItem;
       if (sum === target) {
-        result.push(indexListOne);
-        result.push(indexListTwo);
+        if (firstIndex !== secondIndex) {
+          result.push(firstIndex);
+          result.push(secondIndex);
+          break;
+        }
       }
-    });
-  });
+    }
+    if (result.length === 2) break;
+  }
   console.log('Result:', result);
+
   return result;
+}
+
+// leetCode Solution
+export function twoSumLeetCode(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === target - nums[i]) {
+        return [i, j];
+      }
+    }
+  }
+  // Return an empty array if no solution is found
+  return [];
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { twoSum } from '../solution-1';
 
 function init() {
   twoSum([2, 7, 11, 15], 9);
+  twoSum([3, 2, 4], 6);
+  twoSum([3, 3], 6);
 }
 
 init();
